201c28702e2bbae0aab419cfbc410cdbe84da530
MICHAEL Z. SHIFERAW <michaelzewdu10@gmail.com>
2025-07-19 21:31:21 +0300

    feat: created envVarsSchema and validation

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import dotenv from 'dotenv';
+import Joi from 'joi';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+const envVarsSchema = Joi.object().keys({
+    NODE_ENV: Joi.string()
+        .valid('development', 'production', 'test').required()
+}).unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+    console.error(`Config validation error: ${error}`);
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+export default {
+    env: envVars.NODE_ENV
+};
