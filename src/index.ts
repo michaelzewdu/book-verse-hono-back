66acfcbef0dfa21a276492a6dd47a49e13af24ab
michaelzewdu10 <michaelzewdu10@gmail.com>
2025-07-19 09:21:52 +0000

    fix: replaced express with hono

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import express from 'express';
-const app = express();
+import { Hono } from 'hono';
+const app = new Hono();
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+app.get('/', (c) => {
+  // const name = process.env.NAME || 'World';
+  // res.send(`Hello ${name}!`);
+  return c.text('Hello Hono World!');
 });
 
-const port = parseInt(process.env.PORT || '3000');
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// n
