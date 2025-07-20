5c111265700ef3a6c556728a18e0d3d0d528b772
MICHAEL Z. SHIFERAW <michaelzewdu10@gmail.com>
2025-07-20 14:16:46 +0300

    new: more joi validator scehema for review creation and book review search

--- src/modules/books/validator.ts
+++ src/modules/books/validator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 
-const booksSearchQuery = {
+const booksSearchQuerySchema = {
     query: Joi.object({
         q: Joi.string().min(1).required().messages({
             "string.empty": "Query is required",
@@ -9,4 +9,29 @@ const booksSearchQuery = {
     }),
 };
 
-export { booksSearchQuery };
+const createBookReviewSchema = {
+    body: Joi.object({
+        bookId: Joi.string().min(1).required().messages({
+            "string.empty": "Book ID is required",
+        }),
+        userId: Joi.string().min(1).required().messages({
+            "string.empty": "User ID is required",
+        }),
+        rating: Joi.number().min(1).max(10).required().messages({
+            "string.empty": "Rating is required",
+        }),
+        comment : Joi.string().min(1).required().messages({
+            "string.empty": "Review is required",
+        }),
+    }),
+}
+
+const bookReviewSearchSchema = {
+    params: Joi.object({
+        id: Joi.string().required().messages({
+            "string.empty": "Book id is required to find book review"
+        })
+    })
+}
+
+export { booksSearchQuerySchema, createBookReviewSchema, bookReviewSearchSchema };
