013418553dace48cbf8a010a019bf4430aa3e489
MICHAEL Z. SHIFERAW <michaelzewdu10@gmail.com>
2025-07-20 14:13:18 +0300

    feat: createReviewController and findBookReviewController done

--- src/modules/books/controller.ts
+++ src/modules/books/controller.ts
@@ -1,8 +1,8 @@
-import { newControllerData, newControllerError } from "../../types/controllerResult";
+import { newControllerData, newControllerError } from "@typesdef/controllerResult";
 import booksService from "./service";
 
 const booksQueryController = async (searchQuery) => {
-    // Logic for searching books can be added here
+    // Logic for searching books added here
     try {
 
         const result = await booksService.searchBooks(searchQuery);
@@ -18,4 +18,32 @@ const booksQueryController = async (searchQuery) => {
     }
 }
 
-export default { booksQueryController }; 
+const createReviewController = async (review) => {
+    // Logic for creating a book review can be added here
+    try {
+        const result = await booksService.createReview(review);
+        return newControllerData(
+            result,
+            201
+        );
+    } catch (error) {
+        return newControllerError(
+            error.message || "An error occurred while creating the review", 500
+        );
+    }
+}
+
+const findBookReviewController = async (bookId: string) => {
+
+    try {
+        const result = booksService.findBookReview(bookId);
+        return newControllerData(
+            result,
+            200
+        )
+    } catch (error) {
+        return newControllerError(error.message || "An error occurred while finding book reviews", 500)
+    }
+}
+
+export default { booksQueryController, createReviewController, findBookReviewController }; 
