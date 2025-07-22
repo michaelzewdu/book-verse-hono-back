19d5100326f91f2d05e797fd4c6429abb8e76060
MICHAEL Z. SHIFERAW <michaelzewdu10@gmail.com>
2025-07-22 13:25:28 +0300

    fix: moved logger middleware before auth and graphql
    This way every request will be logged

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import { Hono } from 'hono';
 import routes from '@routes/v1/routes';
-import { bearerAuth } from 'hono/bearer-auth';
 import envVars from '@config/config';
 import { graphqlServer } from '@hono/graphql-server';
 import graphSchema from '@modules/graphql/schema';
 import rootResolver from '@modules/graphql/rootResolver';
 import { fire } from 'hono/service-worker'
 import logger from '@config/logger';
-import * as jose from 'jose';
 import { isAuthenticated } from './middlewares/auth';
 
 const app = new Hono();
+
+app.use('*', async (c, next) => {
+  // Middleware to log the request time, user-agent, method and URL
+  logger.info(`${Date().toString()} ${c.req.method} ${c.req.url} ${c.req.header("User-Agent")}`);
+  return next();
+});
 
 const authenticatedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
 
@@ -19,23 +23,19 @@ const authenticatedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
 // user purchases is also auth protected because it is under the /users/:userId/purchases route
 app.on(authenticatedMethods, ['/users/*', '/books/reviews'], async (c, next) => isAuthenticated(c, next));
 
+
 app.use('/graphql', graphqlServer({
   schema: graphSchema,
   rootResolver: rootResolver,
-  graphiql: true, // Enable GraphiQL interface
+  graphiql: true, // Enable GraphiQL user interface
 }))
 
-fire(app)
+fire(app)  // Register the service worker
 
 app.get('/', (c) => {
   return c.text('Hello Hono World! Welcome to BookVerse API', 200);
 });
 
-app.use('*', async (c, next) => {
-  // Middleware to log the request time, user-agent, method and URL
-  logger.info(`${Date().toString()} ${c.req.method} ${c.req.url} ${c.req.header("User-Agent")}`);
-  return next();
-});
 
 for (const { path, route } of routes) {
   app.route(path, route);
