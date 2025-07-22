5763998f65949eb1c6b8aba675eae96fb8718003
MICHAEL Z. SHIFERAW <michaelzewdu10@gmail.com>
2025-07-22 20:55:02 +0300

    🚚 refactor: moved auth related constants here from index.ts

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -1,5 +1,6 @@
 import booksRouter from '@modules/books/router';
 import {usersRouter, authRouter} from '@modules/users/router';
+
 const routes = [
     {
         path: '/books',
@@ -15,4 +16,10 @@ const routes = [
     }
 ]
 
-export default routes;
+const authenticatedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+
+// Define routes that require authentication
+// These routes will be protected by the isAuthenticated middleware
+const authenticatedRoutes = ['/users/*', '/books/reviews']
+
+export {routes, authenticatedMethods, authenticatedRoutes};
